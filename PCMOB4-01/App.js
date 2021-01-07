db8c56a1a764390685c55d4fa75f2405801f9f5f
snwl123 <weilinneo@gmail.com>
2021-01-08 00:22:52 +0800

    Updated PCMOB4-01

--- PCMOB4-01/App.js
+++ PCMOB4-01/App.js
@@ -1,11 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator } from 'react-native';
 
 export default function App() {
+
+  const [arrival,setArrival] = useState([]);
+  const [busNo,setBusNo] = useState(0);
+
+
+  const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=83139"
+
+  function resetBusStopData()
+  {
+    setArrival("");
+    loadBusStopData();
+  }
+
+  function loadBusStopData()
+  {
+    fetch(BUSSTOP_URL)
+      .then((response) =>
+      {
+        return response.json();
+      })
+      .then((responseData) =>
+      {
+        const myBus = responseData.services.filter
+        ((item) => item.no === "155")[0];
+
+        let newArrival =
+        {
+          arrivalTime: myBus.next.time,
+          arrivalSeconds: myBus.next.duration_ms
+        }
+
+        setArrival
+        ([
+          ... arrival,newArrival
+        ])
+
+        console.log(myBus.next.duration_ms)
+        setBusNo(myBus.no)
+      })
+  }
+
+  useEffect(() => {
+    // const interval = setInterval(loadBusStopData,1000);
+    // return() => clearInterval(interval);
+  },[])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textHeader}>{busNo}</Text>
+      <Text style={styles.textHeader}>Bus Arrival Time</Text>
+      <Text style={styles.textSubheader}>{arrival === [] ? <ActivityIndicator size="small"/> : arrival[0]}</Text>
+      <TouchableOpacity style={styles.button} onPress = {resetBusStopData}>
+        <Text style={styles.buttonText}>Refresh</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +69,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textHeader: {
+    fontSize: 20,
+    marginBottom:10
+  },
+
+  textSubheader: {
+    fontSize: 15,
+    marginBottom: 20
+  },
+
+  button: {
+    backgroundColor: "#ebfaff",
+    borderRadius: 5
+  },
+
+  buttonText: {
+    padding: 10,
+    fontWeight: "600",
+    color: "#333333"
+  },
+
 });
